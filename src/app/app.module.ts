7887f8e67129885f482b0312053342698806e076
Carlos Andrés Burbano <caburbanop@gmail.com>
2020-08-16 16:53:12 -0500

    Avance 	* Se hicieron modificaciones para crear los componentes para cargar 	  el archivo de vídeo. 	* Se hicieron ajustes para agregar botones utilizados para realizar 	  las peticiones al back.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { routing, appRoutingProviders } from './app.routing';
+import { FileSelectDirective, FileDropDirective, FileUploadModule } from 'ng2-file-upload';
 
 import { AppComponent } from './app.component';
+import { CargarComponent } from './components/cargar/cargar.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CargarComponent,
+    HomeComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    routing,
+    HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FileUploadModule
   ],
-  providers: [],
+  providers: [
+    appRoutingProviders
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
